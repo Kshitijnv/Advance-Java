07f43d209fd06019908f11230ec0250eaab4fe82
Kshitij Varshney <101283376+Kshitijnv@users.noreply.github.com>
2023-11-19 12:47:27 +0530

    Delete day4_lab_IPL1/day4_lab_IPL directory

--- day4_lab_IPL1/day4_lab_IPL/src/main/java/dao/PlayerDao.java
+++
@@ -1,8 +0,0 @@
-package dao;
-
-import java.sql.Date;
-import java.sql.SQLException;
-
-public interface PlayerDao {
-	void addPlayer(String abbreviation, String firstName, String lastName, Date dob, double battingAverage, int wicketsTaken) throws SQLException;
-}
